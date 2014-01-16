3db2ea28b2e1993cccdf41336474b7d85f99cb71
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-01-16 18:15:27 -0500

    forgot an equals sign

--- work/resume.php
+++ work/resume.php
@@ -1,12 +1,10 @@
 <?php
-
-$css_paths "/css/work/resume.css";
+$css_paths = "/css/work/resume.css";
 require_once '../header.php';
 ?>
 
 <iframe src="StephenBuckleyResume.htm"></iframe>
 
 <?php
-
 require_once '../footer.php';
 ?>
