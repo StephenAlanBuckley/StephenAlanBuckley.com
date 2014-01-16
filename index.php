f434c8fe30b6b2015224d176bb8a3d09a378bdd5
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-01-16 17:07:04 -0500

    index changing again

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require_once 'header.php';
 <div class="row">
   <img id="portrait" src="/images/head_icon.jpg" class="col-md-8 col-md-offset-3">
 </div>
-<div class="row panel">
-  <a id="work" class="primary_link center col-md-2 col-md-offset-1" href="/work/">Work</a>
-  <a id="play" class="primary_link center col-md-2 col-md-offset-2" href="/play/">Play</a>
-  <a id="think" class="primary_link center col-md-2 col-md-offset-2" href="/think/">Think</a>
+<div class="row pael">
+  <a id="work" class="primary_link text-center col-md-2 col-md-offset-1" href="/work/">Work</a>
+  <a id="play" class="primary_link text-center col-md-2 col-md-offset-2" href="/play/">Play</a>
+  <a id="think" class="primary_link text-center col-md-2 col-md-offset-2" href="/think/">Think</a>
 </div>
 
 <?php
