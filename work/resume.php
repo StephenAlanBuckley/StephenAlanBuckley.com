00a2e55269ab6b77d2d39398092df97ee8ea3552
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-01-16 18:17:34 -0500

    and of course give the iframe the id so the css came up

--- work/resume.php
+++ work/resume.php
@@ -3,7 +3,7 @@ $css_paths = "/css/work/resume.css";
 require_once '../header.php';
 ?>
 
-<iframe src="StephenBuckleyResume.htm"></iframe>
+<iframe id="resume_frame" src="StephenBuckleyResume.htm"></iframe>
 
 <?php
 require_once '../footer.php';
