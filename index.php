13bae50304e9bb6dfd5ee6797bee8aa1e6a9d096
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-01-16 16:56:17 -0500

    index changed to Work/Play/Think

--- index.php
+++ index.php
@@ -4,9 +4,14 @@ $css_paths = "/css/index.css";
 require_once 'header.php';
 ?>
 
-<a id="work" class="primary_link" href="/work/">Work</a>
-<a id="play" class="primary_link" href="/play/">Play</a>
-<a id="think" class="primary_link" href="/think/">Think</a>
+<div class="row">
+  <img id="portrait" src="/images/head_icon.jpg" class="col-md-8 col-md-offset-3">
+</div>
+<div class="row panel">
+  <a id="work" class="primary_link center col-md-2 col-md-offset-1" href="/work/">Work</a>
+  <a id="play" class="primary_link center col-md-2 col-md-offset-2" href="/play/">Play</a>
+  <a id="think" class="primary_link center col-md-2 col-md-offset-2" href="/think/">Think</a>
+</div>
 
 <?php
 require_once 'footer.php';
