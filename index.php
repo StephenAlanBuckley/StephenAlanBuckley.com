3c18c7fb791cbeb73b9a0d9e8edde9dedc874a2d
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-01-16 17:09:54 -0500

    index ch-ch-ch-ch-changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'header.php';
 <div class="row">
   <img id="portrait" src="/images/head_icon.jpg" class="col-md-8 col-md-offset-3">
 </div>
-<div class="row pael">
+<div class="row panel">
   <a id="work" class="primary_link text-center col-md-2 col-md-offset-1" href="/work/">Work</a>
   <a id="play" class="primary_link text-center col-md-2 col-md-offset-2" href="/play/">Play</a>
   <a id="think" class="primary_link text-center col-md-2 col-md-offset-2" href="/think/">Think</a>
