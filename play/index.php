37590dd18b7107ca9d5d64295fba7b6c37a921e5
StephenAlanBuckley <stephen.alan.buckley@gmail.com>
2014-01-22 11:28:09 -0500

    updating play page js and css

--- play/index.php
+++ play/index.php
@@ -1,11 +1,15 @@
 <?php
-$js_paths ="/js/play/index.js";
+$js_paths ="/js/play/index.js,/css/play/index.css";
 require_once '../header.php';
 ?>
 
-<div class="row wheel">
+<div class="wheel">
   <a href="../schedule.php">Comedy Schedule</a>
   <a href="hp_markov.php">Harry Potter Markov Chains</a>
+</div>
+
+<div class="row">
+  <canvas id="ferris_canvas"></canvas>
 </div>
 
 <?php
